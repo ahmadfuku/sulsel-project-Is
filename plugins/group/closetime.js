30496468dd5612085f8dfef8e0913b83e272873c
ahmadfuku <ahmadfuku38@gmail.com>
2026-05-21 23:39:56 +0800

    Upload plugins/group/closetime.js

---
+++ plugins/group/closetime.js
@@ -0,0 +1,24 @@
+import ms from "ms"
+
+let handler = async (m, { fukusima, args }) => {
+    if (!args[0]) return m.reply('Masukkan durasi. Contoh: .closetime 20m / 10s / 2h')
+
+    let time = ms(args[0])
+    if (!time) return m.reply('Format tidak valid. Gunakan: 10s, 5m, 2h')
+
+    m.reply(`Grup akan *ditutup* dalam ${args[0]}`)
+
+    setTimeout(async () => {
+        await fukusima.groupSettingUpdate(m.chat, 'announcement')
+        m.reply('Grup berhasil ditutup otomatis.')
+    }, time)
+}
+
+handler.help = ['closetime <waktu>', 'ctime <waktu>']
+handler.tags = ['group']
+handler.command = /^closetime|ctime$/i
+handler.group = true
+handler.admin = true
+handler.botAdmin = true
+
+export default handler
