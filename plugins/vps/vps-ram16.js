8d7fedc0769650b1680a747f917cb98f02c26543
ahmadfuku <ahmadfuku38@gmail.com>
2026-05-21 23:46:35 +0800

    Upload plugins/vps/vps-ram16.js

---
+++ plugins/vps/vps-ram16.js
@@ -0,0 +1,81 @@
+import fetch from 'node-fetch'
+
+function randomPass() {
+    const chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
+    let pass = ""
+    for (let i = 0; i < 10; i++) pass += chars[Math.floor(Math.random() * chars.length)]
+    return pass
+}
+
+let handler = async (m, { args }) => {
+    const hostname = args[0]
+    if (!hostname) return m.reply('Masukkan hostname VPS!\n\nContoh:\n.vps16g4c nama-vps')
+
+    try {
+        let password = randomPass()
+
+        let body = {
+            name: hostname,
+            region: 'sgp1',
+            size: 's-4vcpu-16gb',
+            image: 'ubuntu-22-04-x64',
+            ssh_keys: null,
+            backups: false,
+            ipv6: true,
+            user_data: `#cloud-config
+password: ${password}
+chpasswd: { expire: False }`,
+            private_networking: null,
+            volumes: null,
+            tags: ['T']
+        }
+
+        let res = await fetch('https://api.digitalocean.com/v2/droplets', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': "Bearer " + global.doToken
+            },
+            body: JSON.stringify(body)
+        })
+
+        let json = await res.json()
+        if (!res.ok) throw new Error(json.message)
+
+        let id = json.droplet.id
+
+        await m.reply("Tunggu sebentar...")
+
+        await new Promise(r => setTimeout(r, 60000))
+
+        let infoRes = await fetch(`https://api.digitalocean.com/v2/droplets/${id}`, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': "Bearer " + global.doToken
+            }
+        })
+
+        let info = await infoRes.json()
+
+        let ip = info?.droplet?.networks?.v4?.length
+            ? info.droplet.networks.v4[0].ip_address
+            : "IP belum tersedia"
+
+        let msg = `VPS berhasil dibuat!\n\n`
+        msg += `ID: ${id}\n`
+        msg += `IP VPS: ${ip}\n`
+        msg += `Password: ${password}\n`
+
+        return m.reply(msg)
+
+    } catch (err) {
+        return m.reply(`Terjadi kesalahan: ${err.message || err}`)
+    }
+}
+
+handler.help = ['vps16g4c']
+handler.tags = ['vps']
+handler.command = /^vps16g4c$/i
+handler.owner = true
+export default handler
