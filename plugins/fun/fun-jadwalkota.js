0b659368e0184f9444228bcf65834afc9cb6dc5c
ahmadfuku <ahmadfuku38@gmail.com>
2026-05-21 23:38:42 +0800

    Upload plugins/fun/fun-jadwalkota.js

---
+++ plugins/fun/fun-jadwalkota.js
@@ -0,0 +1,42 @@
+let handler = async (m, { fukusima, text, command }) => {
+  if (!text) return m.reply(`⚠️ Contoh penggunaan:\n.${command} <kota>\n\nMisal:\n.${command} gresik`)
+
+  let kota = text.trim()
+
+  try {
+    let res = await fetch(`https://api-faa.my.id/faa/jadwal-sholat?kota=${encodeURIComponent(kota)}`)
+    let json = await res.json()
+
+    if (!json.status) return m.reply(`❌ Kota tidak ditemukan.`)
+
+    let hasil = `
+🕌 *JADWAL SHOLAT HARI INI*
+
+📍 Kota: *${json.kota}*
+🗺 Daerah: *${json.daerah}*
+📅 Tanggal: *${json.tanggal}*
+
+🕓 *Imsak:* ${json.jadwal.imsak}
+🕓 *Subuh:* ${json.jadwal.subuh}
+🌅 *Terbit:* ${json.jadwal.terbit}
+🌄 *Dhuha:* ${json.jadwal.dhuha}
+☀ *Dzuhur:* ${json.jadwal.dzuhur}
+🌤 *Ashar:* ${json.jadwal.ashar}
+🌆 *Maghrib:* ${json.jadwal.maghrib}
+🌙 *Isya:* ${json.jadwal.isya}
+
+Creator: Zen
+    `.trim()
+
+    fukusima.sendMessage(m.chat, { text: hasil }, { quoted: m })
+
+  } catch (e) {
+    m.reply("❌ Terjadi kesalahan: " + e)
+  }
+}
+
+handler.command = ["jadwal"]
+handler.tags = ["islami"]
+handler.help = ["jadwal <kota>"]
+
+export default handler
