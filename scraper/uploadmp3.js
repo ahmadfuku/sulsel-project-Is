3abc119a288cf43438944db7c6aaa7eb55bfff08
ahmadfuku <ahmadfuku38@gmail.com>
2026-05-21 23:50:11 +0800

    Upload scraper/uploadmp3.js

---
+++ scraper/uploadmp3.js
@@ -0,0 +1,30 @@
+import fetch from "node-fetch";
+import {
+    FormData,
+    Blob
+} from "formdata-node";
+import { fileTypeFromBuffer } from "file-type";
+
+export async function uploadPomf2(buffer) {
+        try {
+            const {
+                ext,
+                mime
+            } = await fileTypeFromBuffer(buffer) || {};
+            let form = new FormData();
+            const blob = new Blob([buffer.toArrayBuffer()], {
+                type: mime
+            });
+            form.append('files[]', blob, 'tmp.' + ext);
+            let res = await fetch('https://pomf2.lain.la/upload.php', {
+                method: 'POST',
+                body: form
+            });
+            let json = await res.json();
+            if (!json.success) throw json;
+            return json;
+        } catch (error) {
+            console.error(error);
+            throw error;
+        }
+    }
